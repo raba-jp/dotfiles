0aad575e85b2f7ec2bc06a88a97140fcc4c0cf45
Hiroki Sakuraba <sakuraba@tech.ge.jp>
2017-05-19 03:29:34 +0900

    Update

--- config/zsh/packages.zsh
+++ config/zsh/packages.zsh
@@ -3,5 +3,6 @@ zplug "sindresorhus/pure", use:pure.zsh, as:theme
 zplug "zsh-users/zsh-completions", lazy:true
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions", lazy:true
-zplug "mollifier/anyframe"
 zplug "marzocchi/zsh-notify"
+zplug "mollifier/anyframe"
+zplug "rabafea/ot", as:command, use:'(*).sh', rename-to:'$1'
