37944322ef429ab3412477e5f83805156b1a69b9
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-12-25 20:44:15 +0900

    Update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,6 @@
 ## Alias
-alias vi 'vim'
+alias vi 'nvim'
+alias vim 'nvim'
 alias sudo 'sudo '
 alias ls 'exa'
 alias ll 'exa -alhG'
@@ -18,17 +19,20 @@ alias tree 'exa --tree'
 
 # Keybind
 function fish_user_key_bindings
-  bind \cr __select_history
-  bind \ck up-or-search
-  bind \cj down-or-search
-  bind \ch backward-char
-  bind \cl forward-char
+    bind \cr __select_history
+    bind \ck up-or-search
+    bind \cj down-or-search
+    bind \ch backward-char
+    bind \cl forward-char
 end
 
 function done_enter --on-event fish_postexec
-  __do_enter_action $argv
+    __do_enter_action $argv
 end
 
-[ (uname) = 'Darwin' ]; and source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-[ -n (type gcloud) ]; and complete --command gcloud --arguments="($XDG_CONFIG_HOME/fish/gcloud_completion.py (commandline -cp))"
-[ -n (type direnv) ]; and eval (direnv hook fish)
+[ (uname) = 'Darwin' ]
+and source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+[ -n (type gcloud) ]
+and complete --command gcloud --arguments="($XDG_CONFIG_HOME/fish/gcloud_completion.py (commandline -cp))"
+[ -n (type direnv) ]
+and eval (direnv hook fish)
