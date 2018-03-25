a5a79dd04f29c8f9dffa7ac475110138eaac2a0c
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2018-03-26 01:14:25 +0900

    Update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,22 +1,4 @@
 --------------------------------------------------------------------------------
--- CMD + Q を長押しでアプリを終了 
-local qStartTime = 0.0
-local qDuration = 1.5
-
-local function timerStart()
-	qStartTime = hs.timer.secondsSinceEpoch()
-end
-
-local function timerStop()
-	local qEndTime = hs.timer.secondsSinceEpoch()
-	local duration = qEndTime - qStartTime
-	duration >= qDuration and hs.application.frontmostApplication():kill()
-end
-
-longPressQ = hs.hotkey.bind({"cmd"}, "Q", timerStart(), timerStop())
---------------------------------------------------------------------------------
-
---------------------------------------------------------------------------------
 -- 右CMD単体押しでかな、左CMD単体押しで英数切り替え
 --
 --
@@ -26,46 +8,41 @@ longPressQ = hs.hotkey.bind({"cmd"}, "Q", timerStart(), timerStop())
 -- 0x37 Command(left)
 -- 0x36 Command(right)
 local prevKeyCode
+local escape = 0x35
+local leftCommand = 0x37
+local rightCommand = 0x36
+local eisuu = 0x66
+local kana = 0x68
 
 local function keyStroke(modifiers, character)
     hs.eventtap.keyStroke(modifiers, character)
 end
 
-local function japaneseIME()
-	keyStroke({}, 0x68)
+local function jp()
+    keyStroke({}, kana)
 end
 
-local function englishIME()
-	keyStroke({}, 0x66)
-end
-
--- ESCでIMEを英数にする
-local function handleESCKey(code)
-	code == 0x35 and englishIME()
-end
-
-local function handleIMEKey()
-	local isCmdType = e:getFlags()['cmd']
-	local flagsChanged = e:getType() == hs.eventtap.event.types.flagsChanged
-	local isCmdKeyUp = (not isCmdKeyUp) and flagsChanged
-	if isCmdKeyUp and prevKeyCode == 0x37 then
-		englishIME()
-	elseif isCmdKeyU and prevKeyCode == 0x36 then
-		japaneseIME()
-	end
+local function eng()
+    keyStroke({}, eisuu)
 end
 
 local function handleEvent(e)
-	local keyCode = e:getKeyCode()
-	handleESCKey(keyCode)
-	handleIMEKey()
-	prevKeyCode = keyCode
+    local keyCode = e:getKeyCode()
+    if keyCode == escape then
+        eng()
+    end
+
+
+    local isCmdKeyUp = not(e:getFlags()['cmd']) and e:getType() == hs.eventtap.event.types.flagsChanged
+    if isCmdKeyUp and prevKeyCode == leftCommand then
+        eng()
+    elseif isCmdKeyUp and prevKeyCode == rightCommand then
+        jp()
+    end
+
+    prevKeyCode = keyCode
 end
 
-handleIME = hs.eventtap.new({
-	hs.eventtap.event.types.flagsChanged,
-	hs.eventtap.event.types.keyDown,
-	hs.eventtap.event.types.keyUp
-}, handleEvent)
-handleIME:start()
+eventtap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp}, handleEvent)
+eventtap:start()
 --------------------------------------------------------------------------------
