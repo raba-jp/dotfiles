c834c2bbce65b884fd4ddb51360304d2d96ddd50
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2018-04-04 23:58:02 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -u
 
 setup() {
 	local dotfiles_dir=$HOME/.local/share/dotfiles
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	brew install ansible
 	brew install git
 	git clone https://github.com/raba-jp/dotfiles $dotfiles_dir
