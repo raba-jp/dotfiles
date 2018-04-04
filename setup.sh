18b6d22778d0f122b0fd4c5ff08a8987ccbb965e
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2018-04-05 00:09:16 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ setup() {
 	which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	which ansible >/dev/null 2>&1 || brew install ansible
 	which git >/dev/null 2>&1 || brew install git
-	if [ -e $dotfiles_dir ]; then
+	if [ ! -e $dotfiles_dir ]; then
 		git clone https://github.com/raba-jp/dotfiles $dotfiles_dir
 	fi
 	cd $dotfiles_dir
