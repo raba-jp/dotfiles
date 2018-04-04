912a4ec35c6f00bb452dae3cc9a49b178ba56ae6
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2018-04-05 00:07:17 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -6,9 +6,11 @@ set -u
 setup() {
 	local dotfiles_dir=$HOME/.local/share/dotfiles
 	which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	brew install ansible
-	brew install git
-	git clone https://github.com/raba-jp/dotfiles $dotfiles_dir
+	which ansible >/dev/null 2>&1 || brew install ansible
+	which git >/dev/null 2>&1 || brew install git
+	if [ -e $dotfiles_dir ]; then
+		git clone https://github.com/raba-jp/dotfiles $dotfiles_dir
+	fi
 	cd $dotfiles_dir
 	ansible-playbook ansible/inventory ansible/main.yml
 }
