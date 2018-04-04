376b1b970493e05955a0aba0eef068534ac2586d
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2018-04-04 23:51:34 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
-# Thanks, @kaorimatz!
+#!/bin/bash
 
 set -e
 set -u
 
-setup() {}
+setup() {
+	local dotfiles_dir=$HOME/.local/share/dotfiles
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew install ansible
+	brew install git
+	git clone https://github.com/raba-jp/dotfiles $dotfiles_dir
+	cd $dotfiles_dir
+	ansible-playbook ansible/inventory ansible/main.yml
+}
 
 setup
