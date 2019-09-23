0179db13f8139fc9e82e8609d72833d10bc6c68a
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2019-09-23 23:10:37 +0900

    Fix golang error

--- spec/go/go_spec.rb
+++ spec/go/go_spec.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-describe package('go') do
-  it { should be_installed }
-end
-
-describe package('gore') do
-  it { should be_installed }
-end
-
-describe file("#{ENV['HOME']}/dev/src/github.com/mdempsky/gocode") do
-  it { should be_directory }
-end
-
-describe file("#{ENV['HOME']}/dev/src/github.com/k0kubun/pp") do
-  it { should be_directory }
-end
-
-describe package('ghq') do
-  it { should be_installed }
+describe command('go version') do
+  its(:exit_status) { should eq 0 }
 end
 
 describe file("#{ENV['HOME']}/dev/src") do
   it { should be_directory }
+end
+
+describe file("#{ENV['GOPATH']}/bin/gore") do
+  it { should be_file }
+  it { should be_executable }
+end
+
+describe file("#{ENV['GOPATH']}/src/github.com/mdempsky/gocode") do
+  it { should be_directory }
+end
+
+describe file("#{ENV['GOPATH']}/src/github.com/k0kubun/pp") do
+  it { should be_directory }
+end
+
+describe file("#{ENV['GOPATH']}/bin/ghq") do
+  it { should be_file }
+  it { should be_executable }
 end
