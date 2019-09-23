928cc81645406082d732c7ccac6036ba96902915
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2019-09-23 22:20:26 +0900

    Fix ruby install error

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
+package 'git'
+
 node.reverse_merge!(
   rbenv: {
-    user: 'sakuraba',
     global: '2.6.4',
     versions: %w[
       2.6.4
     ]
   }
 )
-
-package 'git'
 
 include_recipe 'rbenv::user'
