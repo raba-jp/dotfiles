b5728c3df846c990e907d9203a6767545f4245df
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2019-09-23 23:40:14 +0900

    Fix error

--- spec/ruby/ruby_spec.rb
+++ spec/ruby/ruby_spec.rb
@@ -5,7 +5,7 @@ describe file('/usr/local/rbenv/bin/rbenv') do
   it { should be_executable }
 end
 
-describe command('/usr/local/rbenv/shims/ruby') do
+describe file('/usr/local/rbenv/shims/ruby') do
   it { should be_file }
   it { should be_executable }
 end
