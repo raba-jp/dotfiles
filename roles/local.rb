786ad008d28c8c2dba42027413bbfd19becb3088
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2019-09-23 17:25:38 +0900

    Fix error

--- roles/local.rb
+++ roles/local.rb
@@ -2,12 +2,12 @@
 
 case node[:platform]
 when 'ubuntu', 'mint'
-  include_recipe 'cookbooks/ubuntu/default'
+  include_recipe '../cookbooks/ubuntu/default'
 when 'darwin'
-  include_recipe 'cookbooks/darwin/default'
+  include_recipe '../cookbooks/darwin/default'
 end
 
-include_recipe 'cookbooks/go/default'
-include_recipe 'cookbooks/python/default'
-include_recipe 'cookbooks/ruby/default'
-include_recipe 'cookbooks/rust/default'
+include_recipe '../cookbooks/go/default'
+include_recipe '../cookbooks/python/default'
+include_recipe '../cookbooks/ruby/default'
+include_recipe '../cookbooks/rust/default'
