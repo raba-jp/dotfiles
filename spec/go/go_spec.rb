7b3402e990c8e399178ae6d9239101c1c441e09b
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2019-09-24 00:07:32 +0900

    Fix go error

--- spec/go/go_spec.rb
+++ spec/go/go_spec.rb
@@ -2,8 +2,30 @@
 
 require 'spec_helper'
 
-describe command('go version') do
-  its(:exit_status) { should eq 0 }
+describe 'go' do
+  describe file('/usr/local/go/bin/go') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/usr/local/bin/go') do
+    it { should be_symlink }
+  end
+end
+
+describe 'gofmt' do
+  describe file('/usr/local/go/bin/gofmt') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/usr/local/bin/gofmt') do
+    it { should be_symlink }
+  end
+
+  describe command('go version') do
+    its(:exit_status) { should eq 0 }
+  end
 end
 
 describe file("#{ENV['HOME']}/dev/src") do
@@ -15,15 +37,15 @@ describe file("#{ENV['GOPATH']}/bin/gore") do
   it { should be_executable }
 end
 
+describe file("#{ENV['GOPATH']}/bin/ghq") do
+  it { should be_file }
+  it { should be_executable }
+end
+
 describe file("#{ENV['GOPATH']}/src/github.com/mdempsky/gocode") do
   it { should be_directory }
 end
 
 describe file("#{ENV['GOPATH']}/src/github.com/k0kubun/pp") do
   it { should be_directory }
-end
-
-describe file("#{ENV['GOPATH']}/bin/ghq") do
-  it { should be_file }
-  it { should be_executable }
 end
