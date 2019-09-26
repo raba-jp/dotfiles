b48eeec52263021bf20f224b80db4a43d13bda78
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2019-09-26 16:13:26 +0900

    Fix error

--- setup.sh
+++ setup.sh
@@ -2,12 +2,15 @@
 
 set -e
 
+cd `dirname $0`
+
 if [ "$(uname)" == 'Darwin' ]; then
   # Mac
   echo "Setup for Mac"
+  echo "Install homebrew"
   which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null 2>&1
+  echo "brew install ansible"
   which ansible >/dev/null 2>&1 || brew install ansible > /dev/null 2>&1
-  which git >/dev/null 2>&1 || brew install git > /dev/null 2>&1
   echo "Setup done"
 fi
 
@@ -15,21 +18,21 @@ if   [ -e /etc/debian_version ] || [ -e /etc/debian_release ]; then
   # Ubuntu
   # Not implemented yet
   echo "Setup for Ubuntu"
+  echo "sudo apt update"
   sudo apt update > /dev/null 2>&1
+  echo "sudo apt install software-properties-common"
   sudo apt install software-properties-common > /dev/null 2>&1
+  echo "sudo apt-add-repository ppa:ansible/ansible"
   sudo apt-add-repository ppa:ansible/ansible > /dev/null 2>&1
+  echo "sudo apt update"
   sudo apt update > /dev/null 2>&1
+  echo "sudo apt install ansible"
   sudo apt install ansible > /dev/null 2>&1
   echo "Setup done"
 fi
 
-dotfiles_dir=$HOME/.local/share/dotfiles
-if [ ! -e $dotfiles_dir ]; then
-  git clone https://github.com/raba-jp/dotfiles $dotfiles_dir
-fi
-cd $dotfiles_dir
 if [ $# -eq 1 ]; then
-  ansible-playbook -i ansible/inventory ansible/main.yml --tags $1
+  ansible-playbook -i inventory main.yml --tags $1
 else
-  ansible-playbook -i ansible/inventory ansible/main.yml
+  ansible-playbook -i inventory main.yml
 fi
