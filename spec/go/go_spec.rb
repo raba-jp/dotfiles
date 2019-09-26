5488f3e436b5b55b1001e5968b062cd370de2c73
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2019-09-26 20:00:26 +0900

    Fix error

--- spec/go/go_spec.rb
+++ spec/go/go_spec.rb
@@ -2,44 +2,26 @@
 
 require 'spec_helper'
 
-describe 'go' do
-  describe file("#{ENV['HOME']}/.local/share/go/bin/go") do
-    it { should be_file }
-    it { should be_executable }
-  end
+describe command('go version') do
+  its(:exit_status) { should eq 0 }
 end
 
-describe 'gofmt' do
-  describe file("#{ENV['HOME']}/.local/share/go/bin/gofmt") do
-    it { should be_file }
-    it { should be_executable }
-  end
-
-  describe command("#{ENV['HOME']}/.local/share/go/bin/go version") do
-    its(:exit_status) { should eq 0 }
-  end
-end
-
-describe file("#{ENV['HOME']}/dev/src") do
-  it { should be_directory }
-end
-
-describe file("#{ENV['GOPATH']}/bin/gore") do
+describe file("#{ENV['HOME']}/dev/bin/gore") do
   it { should be_file }
   it { should be_executable }
 end
 
-describe file("#{ENV['GOPATH']}/bin/ghq") do
+describe file("#{ENV['HOME']}/dev/bin/ghq") do
   it { should be_file }
   it { should be_executable }
 end
 
-describe file("#{ENV['GOPATH']}/src/github.com/mdempsky/gocode") do
+describe file("#{ENV['HOME']}/dev/src/github.com/mdempsky/gocode") do
   it { should be_directory }
   it { should be_owned_by ENV['USER'] }
 end
 
-describe file("#{ENV['GOPATH']}/src/github.com/k0kubun/pp") do
+describe file("#{ENV['HOME']}/dev/src/github.com/k0kubun/pp") do
   it { should be_directory }
   it { should be_owned_by ENV['USER'] }
 end
