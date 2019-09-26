adb1b8ab2217fd284e0501934ef9fcc0dd2abd84
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2019-09-26 18:20:15 +0900

    Fix error

--- spec/clojure/clojure_spec.rb
+++ spec/clojure/clojure_spec.rb
@@ -6,8 +6,12 @@ describe package('clojure') do
   it { should be_installed }
 end
 
-describe file("#{ENV['HOME']}/.config/clojure/deps.edn") do
+describe file("#{ENV['HOME']}/.config/clojure") do
   it { should be_symlink }
+end
+
+describe file("#{ENV['HOME']}/.config/clojure/deps.edn") do
+  it { should be_file }
   it { should be_readable }
   it { should be_owned_by ENV['USER'] }
 end
