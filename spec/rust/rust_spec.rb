03451bc930c6e37ba81a4d3dafeeef03bc766f48
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2019-09-26 18:35:04 +0900

    Fix error

--- spec/rust/rust_spec.rb
+++ spec/rust/rust_spec.rb
@@ -2,6 +2,12 @@
 
 require 'spec_helper'
 
-describe command('rustup --version') do
-  its(:exit_status) { should eq 0 }
+describe file("#{ENV['HOME']}/.local/share/cargo/bin/rustup") do
+  it { should be_file }
+  it { should be_executable }
+end
+
+describe file("#{ENV['HOME']}/.local/share/cargo/bin/cargo") do
+  it { should be_file }
+  it { should be_executable }
 end
