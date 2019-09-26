4288f97805430a8f07799a042f2069a2b5a458cd
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2019-09-26 20:45:11 +0900

    Fix error

--- spec/go/go_spec.rb
+++ spec/go/go_spec.rb
@@ -6,22 +6,29 @@ describe command('go version') do
   its(:exit_status) { should eq 0 }
 end
 
-describe file("#{ENV['HOME']}/dev/bin/gore") do
-  it { should be_file }
-  it { should be_executable }
+%w[
+  direnv
+  gore
+  gocode
+  gopls
+  ghq
+].each do |pkg|
+  describe file("#{ENV['HOME']}/dev/bin/#{pkg}") do
+    it { should be_file }
+    it { should be_executable }
+  end
 end
 
-describe file("#{ENV['HOME']}/dev/bin/ghq") do
-  it { should be_file }
-  it { should be_executable }
-end
-
-describe file("#{ENV['HOME']}/dev/src/github.com/mdempsky/gocode") do
-  it { should be_directory }
-  it { should be_owned_by ENV['USER'] }
-end
-
-describe file("#{ENV['HOME']}/dev/src/github.com/k0kubun/pp") do
-  it { should be_directory }
-  it { should be_owned_by ENV['USER'] }
+[
+  'github.com/direnv/direnv',
+  'github.com/motemen/gore',
+  'github.com/k0kubun/pp',
+  'github.com/mdempsky/gocode',
+  'golang.org/x/tools/cmd/gopls',
+  'github.com/motemen/ghq'
+].each do |pkg|
+  describe file("#{ENV['HOME']}/dev/src/#{pkg}") do
+    it { should be_directory }
+    it { should be_owned_by ENV['USER'] }
+  end
 end
