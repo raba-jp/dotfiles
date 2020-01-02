c7a787816986222185a9c0fbdba22b89191380af
Hiroki Sakuraba <feath.7615@gmail.com>
2020-01-03 00:56:36 +0900

    Update

--- config/zsh/packages.zsh
+++
@@ -1,8 +0,0 @@
-zplug "mafredri/zsh-async", use:async.zsh
-zplug "sindresorhus/pure", use:pure.zsh, as:theme
-zplug "zsh-users/zsh-completions", lazy:true
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "zsh-users/zsh-autosuggestions", lazy:true
-zplug "marzocchi/zsh-notify"
-zplug "mollifier/anyframe"
-zplug "rabafea/ot", as:command, use:'(*).sh', rename-to:'$1'
