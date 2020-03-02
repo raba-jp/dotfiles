00e1d9af194deea4b96197e8ab7654b72aab5e31
Hiroki Sakuraba <feath.7615@gmail.com>
2020-03-02 19:17:43 +0900

    Update

--- cookbooks/rust/default.rb
+++ cookbooks/rust/default.rb
@@ -1,4 +1,4 @@
-execute "bash -c 'curl https://sh.rustup.rs -sSf | sh -s -- -y" do
+execute `bash -c 'curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y` do
   user node["user"]
   not_if "type cargo"
 end
