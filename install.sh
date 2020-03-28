5a6c4605d5e249963ace8de330d14775b9ef3989
Hiroki Sakuraba <feath.7615@gmail.com>
2020-03-28 18:23:07 +0900

    Update

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ cd $(dirname $0)
 ./bin/setup_mitamae
 
 if [ "$(uname)" = 'Darwin' ]; then
-	./bin/mitamae local -j darwin.json ./bootstrap.rb
+	./bin/mitamae local -j ./nodes/darwin.json ./bootstrap.rb
 fi
 
 if [ -e /etc/arch-release ]; then
-	sudo ./bin/mitamae local -j arch.json ./bootstrap.rb
+	sudo ./bin/mitamae local -j ./nodes/arch.json ./bootstrap.rb
 fi
