cd1d767bd8581f36415b73ee3d0a3cd7f72fa795
Hiroki Sakuraba <feath.7615@gmail.com>
2020-04-17 15:45:16 +0900

    Update

--- definitions/pkg.rb
+++ definitions/pkg.rb
@@ -5,8 +5,13 @@ define :pkg do
       not_if "yay -Q #{name} || yay -Qg #{name}"
       user node["user"]
     end
-  ulsif darwin?
-    execute "brew #{name}"
+  elsif darwin?
+    cask = name.start_with? "cask"
+    pkg_name = name.gsub("cask ")
+    execute "brew cask install #{pkg_name}" do
+      not_if "brew cask list | grep #{pkg_name}"
+      user node["user"]
+    end
   else
     package name do
       user params[:user]
