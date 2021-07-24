90ae1c323be41600735f9b2eb43b937c2f0de6e3
Hiroki Sakuraba <feath.7615@gmail.com>
2021-07-24 18:51:25 +0900

    Update

--- home/hammerspoon/default.nix
+++ home/hammerspoon/default.nix
@@ -8,4 +8,6 @@ in {
   xdg.configFile."hammerspoon/init.lua".text = initFile;
   xdg.configFile."hammerspoon/Spoons/CMDKanaEng.spoon/init.lua".text = cmdKanaEngFile;
   xdg.configFile."hammerspoon/Spoons/CtrlMate.spoon/init.lua".text = ctrlMateFile;
+
+  # https://github.com/Hammerspoon/hammerspoon/pull/582
 }
