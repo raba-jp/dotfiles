3f5024ba9cb22c9d0a8e4893c5544d99979dedad
Hiroki Sakuraba <feath.7615@gmail.com>
2021-11-11 18:11:11 +0900

    Update

---
+++ bin/install-nix.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+URL="https://nixos.org/nix/install"
+
+# install using workaround for darwin systems
+[[ $(uname -s) = "Darwin" ]] && FLAG="--darwin-use-unencrypted-nix-store-volume"
+[[ ! -z "$1" ]] && URL="$1"
+
+if command -v nix > /dev/null; then
+    echo "nix is already installed on this system."
+else
+    bash <(curl -L $URL) --daemon $FLAG
+    nix-env -iA nixpkgs.nix_2_4
+fi
