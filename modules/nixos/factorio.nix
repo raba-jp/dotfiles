f8d023da9e16f39e037d05170549dfb0c7547a9e
Hiroki Sakuraba <feath.7615@gmail.com>
2021-11-17 14:42:49 +0900

    Update

--- modules/nixos/factorio.nix
+++ modules/nixos/factorio.nix
@@ -2,13 +2,14 @@
 let
   userFile = builtins.readFile ../../secrets/factorio_username;
   tokenFile = builtins.readFile ../../secrets/factorio_token;
-in lib.mkIf (builtins.pathExists ../../secrets/factorio_username) {
-  services.factorio = {
-    enable = true;
+in lib.mkIf ((builtins.hashFile "sha256" ../../secrets/factorio_username)
+  == "34fc592b9e318b3bc7931a26ebc6eef899990251eded749ec9a10e9d43cc7eda") {
+    services.factorio = {
+      enable = true;
 
-    username = userFile;
-    token = tokenFile;
+      username = userFile;
+      token = tokenFile;
 
-    public = false;
-  };
-}
+      public = false;
+    };
+  }
