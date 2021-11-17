fc29318d8a984f103a52391194eb8c2122e8be25
Hiroki Sakuraba <feath.7615@gmail.com>
2021-11-17 21:51:13 +0900

    Update

--- modules/nixos/factorio.nix
+++ modules/nixos/factorio.nix
@@ -2,7 +2,7 @@
 let
   userFile = builtins.readFile ../../secrets/factorio_username;
   tokenFile = builtins.readFile ../../secrets/factorio_token;
-in lib.mkIf ((builtins.hashFile "sha256" ../../secrets/factorio_username)
+in lib.mkIf ((builtins.hashFile "sha256" ../../secrets/factorio_token)
   == "34fc592b9e318b3bc7931a26ebc6eef899990251eded749ec9a10e9d43cc7eda") {
     services.factorio = {
       enable = true;
