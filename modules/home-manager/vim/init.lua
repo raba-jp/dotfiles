0809f5d1b66e3b27884e0b8986d28899d396a972
Hiroki Sakuraba <feath.7615@gmail.com>
2022-01-19 11:27:52 +0900

    Update

--- modules/home-manager/vim/init.lua
+++ modules/home-manager/vim/init.lua
@@ -134,6 +134,35 @@ telescope.setup({
 			override_file_sorter = true,
 			case_mode = "smart_case",
 		},
+
+		command_palette = {
+			{
+				"File",
+				{ "Files", ":lua require('telescope.builtin').find_files()", 1 },
+				{ "File browser", ":lua require('telescope.builtin').file_browser()", 1 },
+				{ "Git files", ":lua require('telescope.builtin').git_files()", 1 },
+			},
+			{
+				"Navigation",
+				{ "Buffers", ":lua require('telescope.builtin').buffers()", 1 },
+				{ "Search", ":lua require('telescope.builtin').current_buffer_fuzzy_find()", 1 },
+			},
+			{
+				"LSP",
+				{ "References", ":lua require('telescope.builtin').lsp_references()", 1 },
+				{ "Document Symbols", ":lua require('telescope.builtin').lsp_document_symbols()", 1 },
+				{ "Document Diagnostics", ":lua require('telescope.builtin').lsp_document_diagnostics()", 1 },
+				{ "Workspace Symbols", ":lua require('telescope.builtin').lsp_workspace_symbols()", 1 },
+				{ "Workspace Diagnostics", ":lua require('telescope.builtin').lsp_document_diagnostics", 1 },
+				{ "Definitions", ":lua require('telescope.builtin').lsp_definitions()", 1 },
+			},
+			{
+				"Vim",
+				{ "Commands", ":lua require('telescope.builtin').commands()", 1 },
+				{ "Registers", ":lua require('telescope.builtin').registers()", 1 },
+			},
+			{ "Help", { "Show cheatsheet", ":Cheatsheet", 1 }, { "Edit cheatsheet", ":CheatsheetEdit", 1 } },
+		},
 	},
 })
 telescope.load_extension("ghq")
@@ -141,35 +170,6 @@ telescope.load_extension("command_palette")
 telescope.load_extension("fzf")
 
 require("octo").setup()
-
-CpMenu = {
-	{
-		"File",
-		{ "Files", ":lua require('telescope.builtin').find_files()" },
-		{ "File browser", ":lua require('telescope.builtin').file_browser()" },
-		{ "Git files", ":lua require('telescope.builtin').git_files()" },
-	},
-	{
-		"Navigation",
-		{ "Buffers", ":lua require('telescope.builtin').buffers()" },
-		{ "Search", ":lua require('telescope.builtin').current_buffer_fuzzy_find()" },
-	},
-	{
-		"LSP",
-		{ "References", ":lua require('telescope.builtin').lsp_references()" },
-		{ "Document Symbols", ":lua require('telescope.builtin').lsp_document_symbols()" },
-		{ "Document Diagnostics", ":lua require('telescope.builtin').lsp_document_diagnostics()" },
-		{ "Workspace Symbols", ":lua require('telescope.builtin').lsp_workspace_symbols()" },
-		{ "Workspace Diagnostics", ":lua require('telescope.builtin').lsp_document_diagnostics" },
-		{ "Definitions", ":lua require('telescope.builtin').lsp_definitions()" },
-	},
-	{
-		"Vim",
-		{ "Commands", ":lua require('telescope.builtin').commands()" },
-		{ "Registers", ":lua require('telescope.builtin').registers()" },
-	},
-	{ "Help", { "Show cheatsheet", ":Cheatsheet" }, { "Edit cheatsheet", ":CheatsheetEdit" } },
-}
 
 require("lualine").setup({ options = { theme = "nord" } })
 
