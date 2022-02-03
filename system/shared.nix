1f36e47ef82f69a88e8b07dedafd35c05c4bd880
Hiroki Sakuraba <feath.7615@gmail.com>
2022-02-03 09:31:57 +0900

    Update

--- system/shared.nix
+++ system/shared.nix
@@ -18,5 +18,15 @@
 
     maxJobs = 8;
     buildCores = 8;
+
+    binaryCaches = [
+      "https://raba-jp.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    binaryCachePublicKeys = [
+      "raba-jp.cachix.org-1:NgVIMhL5fUaEclOsEtMnCBbyrYDG+qvPPldf2pqklu8="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 }
