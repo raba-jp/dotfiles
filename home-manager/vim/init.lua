0f25b1a2a555cb2b22bf002529fa965f130f230e
Hiroki Sakuraba <feath.7615@gmail.com>
2022-03-03 19:02:14 +0900

    Update

--- home-manager/vim/init.lua
+++ home-manager/vim/init.lua
@@ -21,22 +21,6 @@ setmap("", "<C-k>", "<Plug>(edgemotion-k)", {})
 setmap("n", "<ESC><ESC>", ":nohlsearch<CR>", { noremap = true, silent = true })
 setmap("i", "<Tab>", 'pumvisible() ? "<C-n>" : "<Tab>"', { noremap = true, expr = true })
 setmap("i", "<S-Tab>", 'pumvisible() ? "<C-p>" : "<S-Tab>"', { noremap = true, expr = true })
-setmap("n", "sh", '<cmd>lua require"lspsaga.provider".lsp_finder()<CR>', { silent = true, noremap = true })
-setmap("n", "sa", '<cmd>lua require"lspsaga.codeaction".code_action()<CR>', { silent = true, noremap = true })
-setmap("v", "sa", ':<C-U>lua require"lspsaga.codeaction".range_code_action()<CR>', { silent = true, noremap = true })
-setmap("n", "sk", '<cmd>lua require"lspsaga.hover".render_hover_doc()<CR>', { silent = true, noremap = true })
-setmap(
-	"n",
-	"<C-f>",
-	'<cmd>lua require"lspsaga.action".smart_scroll_with_saga(1)<CR>',
-	{ silent = true, noremap = true }
-)
-setmap(
-	"n",
-	"<C-b>",
-	'<cmd>lua require"lspsaga.action".smart_scroll_with_saga(-1)<CR>',
-	{ silent = true, noremap = true }
-)
 
 gvar.mapleader = " "
 setmap(
