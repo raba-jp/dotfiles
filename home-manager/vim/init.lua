e73f6c4d9a442317303cd3f66657073d5940e1b3
Hiroki Sakuraba <feath.7615@gmail.com>
2022-04-01 16:08:05 +0900

    Update

--- home-manager/vim/init.lua
+++ home-manager/vim/init.lua
@@ -1,3 +1,22 @@
+-- Disable default plugins
+vim.g.loaded_gzip = false
+vim.g.loaded_tar = false
+vim.g.loaded_tarPlugin = false
+vim.g.loaded_zip = false
+vim.g.loaded_zipPlugin = false
+vim.g.loaded_rrhelper = false
+vim.g.loaded_vimball = false
+vim.g.loaded_vimballPlugin = false
+vim.g.loaded_getscript = false
+vim.g.loaded_getscriptPlugin = false
+vim.g.loaded_netrw = false
+vim.g.loaded_netrwPlugin = false
+vim.g.loaded_netrwSettings = false
+vim.g.loaded_netrwFileHandlers = false
+vim.g.did_install_default_menus = false
+vim.g.skip_loading_mswin = false
+vim.g.did_install_syntax_menu = false
+
 -- Keybindings
 vim.api.nvim_set_keymap("i", "jj", "<ESC>", { silent = true })
 vim.api.nvim_set_keymap("n", " ", "<Nop>", { noremap = true })
