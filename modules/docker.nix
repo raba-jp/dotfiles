3641421e95b5a43cac6ccab5daab30f79272ec6a
Hiroki Sakuraba <feath.7615@gmail.com>
2022-04-11 01:09:09 +0900

    Update

--- modules/docker.nix
+++ modules/docker.nix
@@ -3,12 +3,12 @@ with lib;
 let
   cfg = config.dotfiles.docker;
 in
-  {
-    options.dotfiles.docker = {
-      enable = mkEnableOption "if you need docker";
-    };
+{
+  options.dotfiles.docker = {
+    enable = mkEnableOption "if you need docker";
+  };
 
-    config = mkIf cfg.enable {
-      virtualisation.docker.enable = true;
-    };
-  }
+  config = mkIf cfg.enable {
+    virtualisation.docker.enable = true;
+  };
+}
