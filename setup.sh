4573f25afd034d6058fede4e6b09103909c87f97
Hiroki Sakuraba <feath.7615@gmail.com>
2022-04-24 11:29:59 +0000

    Update

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ${SCRIPT_DIR}/scripts/install-nix.sh
 
 if [ "$CODESPACES" = "true" ]; then
+  . /home/codespace/.nix-profile/etc/profile.d/nix.sh
 	nix build --no-link '.#homeConfigurations.vscode.activationPackage'
-	"$(nix path-info '.#homeConfigurations.vscode.activationPackage)"/activate'
+	"$(nix path-info '.#homeConfigurations.vscode.activationPackage')"/activate
 fi
