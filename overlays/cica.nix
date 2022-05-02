2d2e2928a4d5a2b5dfa07803fb468610020822ed
Hiroki Sakuraba <feath.7615@gmail.com>
2022-05-02 20:38:04 +0900

    Update

--- overlays/cica.nix
+++ overlays/cica.nix
@@ -1,14 +1,13 @@
 final: prev:
-let version = "5.0.2";
+let version = "5.0.3";
 in
 {
   cica = prev.fetchzip {
     name = "cica-${version}";
 
     url =
-      "https://github.com/miiton/Cica/releases/download/v${version}/Cica_v${version}_with_emoji.zip";
-    sha256 = "0zh1n5x1swgh72pyzgkvajs9r4xn172v6shl7978gjbh9xlqb8a2";
-
+      "https://github.com/miiton/Cica/releases/download/v${version}/Cica_v${version}.zip";
+    sha256 = "0vshn2cd70mnbavsw9cbagcasa95wiv9qdj4wkzxn7gxygqvrlfb";
     postFetch = ''
       mkdir -p $out/share/fonts/cica
       unzip -j $downloadedFile \*.ttf -d $out/share/fonts/cica
