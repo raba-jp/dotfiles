9e1ff185ea8b9bc1395f8b7c06e56c3992ee9cbc
github-action-benchmark <github@users.noreply.github.com>
2022-05-06 06:56:40 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 995b012ca297b6427c675753cd1711a4796b087b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1651771900354,
+  "lastUpdate": 1651820200505,
   "repoUrl": "https://github.com/raba-jp/dotfiles",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "vim startup time (min)",
             "value": "134.771000",
+            "unit": "msec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "feath.7615@gmail.com",
+            "name": "Hiroki Sakuraba",
+            "username": "raba-jp"
+          },
+          "committer": {
+            "email": "feath.7615@gmail.com",
+            "name": "Hiroki Sakuraba",
+            "username": "raba-jp"
+          },
+          "distinct": true,
+          "id": "995b012ca297b6427c675753cd1711a4796b087b",
+          "message": "Update",
+          "timestamp": "2022-05-06T15:52:53+09:00",
+          "tree_id": "c66a2762ddb61c08adfee4cae0c6783acae78778",
+          "url": "https://github.com/raba-jp/dotfiles/commit/995b012ca297b6427c675753cd1711a4796b087b"
+        },
+        "date": 1651820199554,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "fish startup time (average)",
+            "value": "25.483627319335938",
+            "unit": "msec"
+          },
+          {
+            "name": "vim startup time (average)",
+            "value": "148.114000",
+            "unit": "msec"
+          },
+          {
+            "name": "vim startup time (max)",
+            "value": "152.858000",
+            "unit": "msec"
+          },
+          {
+            "name": "vim startup time (min)",
+            "value": "142.816000",
             "unit": "msec"
           }
         ]
