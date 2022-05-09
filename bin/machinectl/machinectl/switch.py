90f7e14fcf5cd73508d76f2fff57b4b03962912c
Hiroki Sakuraba <feath.7615@gmail.com>
2022-05-09 14:42:26 +0900

    Update

--- bin/machinectl/machinectl/switch.py
+++ bin/machinectl/machinectl/switch.py
@@ -8,5 +8,6 @@ app = typer.Typer()
 def switch():
     subprocess.run("sudo nixos-rebuild switch --flake path:.#define7")
 
-    subprocess.run("nix build './#darwinConfigurations.LF2107010038.system'")
-    subprocess.run("./result/sw/bin/darwin-rebuild switch --flake ./")
+    link = subprocess.run("nix build './#darwinConfigurations.LF2107010038.system' --no-out-link")
+    switch = "{link}/bin/darwin-rebuild switch --flake ./".format(link=link)
+    subprocess.run(switch)
