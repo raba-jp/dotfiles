090ef6cdca76826ce9559a195f9f3e25227ff015
Hiroki Sakuraba <feath.7615@gmail.com>
2022-05-09 16:58:35 +0900

    Update

--- home-manager/vim/init.lua
+++ home-manager/vim/init.lua
@@ -59,179 +59,176 @@ local function global_options()
 	vim.api.nvim_set_keymap("i", "<S-Tab>", 'pumvisible() ? "<C-p>" : "<S-Tab>"', { noremap = true, expr = true })
 end
 
-disable_default_plugins()
-global_options()
+local function colorscheme()
+	require("nightfox").setup({
+		modules = {
+			["cmp"] = true,
+			["gitsigns"] = true,
+			["lsp_saga"] = true,
+			["native_lsp"] = true,
+			["notify"] = true,
+			["telescope"] = true,
+			["treesitter"] = true,
+		},
+	})
+	vim.cmd("colorscheme nordfox")
+end
 
-vim.notify = require("notify")
-require("gitsigns").setup()
+local function lsp()
+	local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+	local lspconfig = require("lspconfig")
+	local lspformat = require("lsp-format")
 
--- Colorscheme
-vim.cmd("colorscheme nordfox")
-require("nightfox").setup({
-	modules = {
-		["cmp"] = true,
-		["gitsigns"] = true,
-		["lsp_saga"] = true,
-		["native_lsp"] = true,
-		["notify"] = true,
-		["telescope"] = true,
-		["treesitter"] = true,
-	},
-})
+	local default_lsp_options = { capabilities = capabilities, on_attach = lspformat.on_attach }
 
--- LSP
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local lspconfig = require("lspconfig")
-local lspformat = require("lsp-format")
-
-local default_lsp_options = { capabilities = capabilities, on_attach = lspformat.on_attach }
-
-lspconfig.gopls.setup(default_lsp_options)
-lspconfig.rust_analyzer.setup(default_lsp_options)
-lspconfig.rnix.setup(default_lsp_options)
-lspconfig.pyright.setup(default_lsp_options)
-lspconfig.dartls.setup(default_lsp_options)
-lspconfig.solargraph.setup(default_lsp_options)
-lspconfig.terraformls.setup(default_lsp_options)
-lspconfig.tsserver.setup(default_lsp_options)
-lspconfig.yamlls.setup({
-	capabilities = capabilities,
-	on_attach = lspformat.on_attach,
-	settings = {
-		yaml = {
-			schemaStore = {
-				enable = true,
+	lspconfig.gopls.setup(default_lsp_options)
+	lspconfig.rust_analyzer.setup(default_lsp_options)
+	lspconfig.rnix.setup(default_lsp_options)
+	lspconfig.pyright.setup(default_lsp_options)
+	lspconfig.dartls.setup(default_lsp_options)
+	lspconfig.solargraph.setup(default_lsp_options)
+	lspconfig.terraformls.setup(default_lsp_options)
+	lspconfig.tsserver.setup(default_lsp_options)
+	lspconfig.yamlls.setup({
+		capabilities = capabilities,
+		on_attach = lspformat.on_attach,
+		settings = {
+			yaml = {
+				schemaStore = {
+					enable = true,
+				},
 			},
 		},
-	},
-})
-lspconfig.sumneko_lua.setup({
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
+	})
+	lspconfig.sumneko_lua.setup({
+		settings = {
+			Lua = {
+				runtime = {
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { "vim" },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
 			},
 		},
-	},
-	capabilities = capabilities,
-})
-lspconfig.efm.setup({
-	on_attach = require("lsp-format").on_attach,
-	init_options = { documentFormatting = true },
-	filetypes = { "lua" },
-	settings = {
-		languages = {
-			lua = {
+		capabilities = capabilities,
+	})
+	lspconfig.efm.setup({
+		on_attach = require("lsp-format").on_attach,
+		init_options = { documentFormatting = true },
+		filetypes = { "lua" },
+		settings = {
+			languages = {
+				lua = {
+					{
+						formatCommand = [[stylua -]],
+						formatStdin = true,
+					},
+				},
+			},
+		},
+	})
+end
+
+local function treesitter()
+	require("nvim-treesitter.configs").setup({
+		sync_install = false,
+		highlight = { enable = true },
+		indent = { enable = true },
+		lsp_interop = { enable = true },
+		refactor = {
+			navigation = { enable = true },
+			highlight_definitions = { enable = true },
+			highlight_current_scope = { enable = true },
+		},
+		rainbow = { enable = true },
+	})
+	require("treesitter-context").setup({ enable = true, throttle = true })
+
+	local gps = require("nvim-gps")
+	gps.setup({
+		["class-name"] = " ",
+		["function-name"] = " ",
+		["method-name"] = " ",
+		["container-name"] = "⛶ ",
+		["tag-name"] = "炙",
+	})
+
+	local colors = {
+		nord0 = "#2E3440",
+		nord1 = "#3B4252",
+		nord2 = "#434C5E",
+		nord3 = "#4C566A",
+		nord5 = "#E5E9F0",
+		nord6 = "#ECEFF4",
+		nord7 = "#8FBCBB",
+		nord8 = "#88C0D0",
+		nord13 = "#EBCB8B",
+	}
+	local lualine_theme = {
+		normal = {
+			a = { fg = colors.nord1, bg = colors.nord8, gui = "bold" },
+			b = { fg = colors.nord5, bg = colors.nord2 },
+			c = { fg = colors.nord5, bg = colors.nord0 },
+		},
+		insert = { a = { fg = colors.nord1, bg = colors.nord6, gui = "bold" } },
+		visual = { a = { fg = colors.nord1, bg = colors.nord7, gui = "bold" } },
+		replace = { a = { fg = colors.nord1, bg = colors.nord13, gui = "bold" } },
+		inactive = {
+			a = { fg = colors.nord1, bg = colors.nord8, gui = "bold" },
+			b = { fg = colors.nord5, bg = colors.nord1 },
+			c = { fg = colors.nord5, bg = colors.nord1 },
+		},
+	}
+
+	require("lualine").setup({
+		options = {
+			theme = lualine_theme,
+			component_separators = "|",
+			section_separators = { left = "", right = "" },
+		},
+		sections = {
+			lualine_a = {
+				{ "mode", separator = { left = "" }, right_padding = 2 },
+			},
+			lualine_b = {
+				{ "filetype", icon_only = true, separator = { right = "" } },
+				"filename",
+			},
+			lualine_c = {
+				{ gps.get_location, cond = gps.is_available },
+			},
+			lualine_x = {
 				{
-					formatCommand = [[stylua -]],
-					formatStdin = true,
+					"diagnostics",
+					sources = { "nvim_diagnostic", "nvim_lsp" },
+					section = { "error", "warn", "info", "hint" },
+					diagnostics_color = {
+						error = "DiagnosticError",
+						warn = "DiagnosticWarn",
+						info = "DiagnosticInfo",
+						hint = "DiagnosticHint",
+					},
+					symbols = { error = " ", warn = " ", info = " ", hint = " " },
+					colored = true,
+					update_in_insert = true,
+					always_visible = false,
 				},
 			},
-		},
-	},
-})
-
--- Treesitter
-require("nvim-treesitter.configs").setup({
-	sync_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },
-	lsp_interop = { enable = true },
-	refactor = {
-		navigation = { enable = true },
-		highlight_definitions = { enable = true },
-		highlight_current_scope = { enable = true },
-	},
-	rainbow = { enable = true },
-})
-require("treesitter-context").setup({ enable = true, throttle = true })
-
-local gps = require("nvim-gps")
-gps.setup({
-	["class-name"] = " ",
-	["function-name"] = " ",
-	["method-name"] = " ",
-	["container-name"] = "⛶ ",
-	["tag-name"] = "炙",
-})
-
-local colors = {
-	nord0 = "#2E3440",
-	nord1 = "#3B4252",
-	nord2 = "#434C5E",
-	nord3 = "#4C566A",
-	nord5 = "#E5E9F0",
-	nord6 = "#ECEFF4",
-	nord7 = "#8FBCBB",
-	nord8 = "#88C0D0",
-	nord13 = "#EBCB8B",
-}
-local lualine_theme = {
-	normal = {
-		a = { fg = colors.nord1, bg = colors.nord8, gui = "bold" },
-		b = { fg = colors.nord5, bg = colors.nord2 },
-		c = { fg = colors.nord5, bg = colors.nord0 },
-	},
-	insert = { a = { fg = colors.nord1, bg = colors.nord6, gui = "bold" } },
-	visual = { a = { fg = colors.nord1, bg = colors.nord7, gui = "bold" } },
-	replace = { a = { fg = colors.nord1, bg = colors.nord13, gui = "bold" } },
-	inactive = {
-		a = { fg = colors.nord1, bg = colors.nord8, gui = "bold" },
-		b = { fg = colors.nord5, bg = colors.nord1 },
-		c = { fg = colors.nord5, bg = colors.nord1 },
-	},
-}
-
-require("lualine").setup({
-	options = {
-		theme = lualine_theme,
-		component_separators = "|",
-		section_separators = { left = "", right = "" },
-	},
-	sections = {
-		lualine_a = {
-			{ "mode", separator = { left = "" }, right_padding = 2 },
-		},
-		lualine_b = {
-			{ "filetype", icon_only = true, separator = { right = "" } },
-			"filename",
-		},
-		lualine_c = {
-			{ gps.get_location, cond = gps.is_available },
-		},
-		lualine_x = {
-			{
-				"diagnostics",
-				sources = { "nvim_diagnostic", "nvim_lsp" },
-				section = { "error", "warn", "info", "hint" },
-				diagnostics_color = {
-					error = "DiagnosticError",
-					warn = "DiagnosticWarn",
-					info = "DiagnosticInfo",
-					hint = "DiagnosticHint",
-				},
-				symbols = { error = " ", warn = " ", info = " ", hint = " " },
-				colored = true,
-				update_in_insert = true,
-				always_visible = false,
+			lualine_y = {
+				{ "location", separator = { left = "", right = "" }, left_padding = 2 },
 			},
+			lualine_z = {},
 		},
-		lualine_y = {
-			{ "location", separator = { left = "", right = "" }, left_padding = 2 },
-		},
-		lualine_z = {},
-	},
-})
+	})
+end
 
--- Telescope
+local function telescope()
 local actions = require("telescope.actions")
 require("telescope").setup({
 	defaults = {
@@ -277,6 +274,17 @@ require("telescope").setup({
 })
 require("telescope").load_extension("ghq")
 require("telescope").load_extension("fzf")
+end
+
+disable_default_plugins()
+global_options()
+colorscheme()
+lsp()
+treesitter()
+telescope()
+
+vim.notify = require("notify")
+require("gitsigns").setup()
 
 local cmp = require("cmp")
 local luasnip = require("luasnip")
