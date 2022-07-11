f55899fd24f287472e4c70e54aac2b9c14b6e149
Hiroki Sakuraba <feath.7615@gmail.com>
2022-07-12 02:36:58 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -5,12 +5,11 @@ SCRIPT_DIR=$(
 	pwd
 )
 
-# Configure Nix
-add_config() {
-	echo "$1" | tee -a "$workdir/nix.conf" >/dev/null
-}
+workdir=$(mktemp -d)
+trap 'rm -rf "$workdir"' EXIT
 
 if [ "$CODESPACES" = "true" ]; then
+
 	mkdir -m 0755 /nix
 	chown codespace /nix
 	groupadd nixbld
@@ -19,13 +18,13 @@ if [ "$CODESPACES" = "true" ]; then
 		useradd -c "Nix build user $n" -d /var/empty -g nixbld -G nixbld -M -N -r -s "$(command -v nologin)" "nixbld$n"
 	done
 
-	add_config "max-jobs = auto"
-	add_config "trusted-users = root $USER"
-	add_config "experimental-features = nix-command flakes"
+	echo "max-jobs = auto" | tee -a "$workdir/nix.conf" >/dev/null
+	echo "trusted-users = root $USER" | tee -a "$workdir/nix.conf" >/dev/null
+	echo "experimental-features = nix-command flakes" | tee -a "$workdir/nix.conf" >/dev/null
 
 	curl -o /tmp/nix-install.sh -L https://nixos.org/nix/install
 	chmod +x /tmp/nix-install.sh
-	/tmp/nix-install.sh --no-daemon
+	/tmp/nix-install.sh --no-daemon --nix-extra-conf-file "$workdir/nix.conf"
 	. /home/codespace/.nix-profile/etc/profile.d/nix.sh
 	nix build --no-link '.#homeConfigurations.codespace.activationPackage'
 	"$(nix path-info '.#homeConfigurations.codespace.activationPackage')"/activate
