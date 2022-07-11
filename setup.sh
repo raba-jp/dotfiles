887f716a624fb42e0c2f476b87ea8b34f79d09f7
Hiroki Sakuraba <feath.7615@gmail.com>
2022-07-12 02:50:00 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -18,16 +18,12 @@ if [ "$CODESPACES" = "true" ]; then
 		sudo useradd -c "Nix build user $n" -d /var/empty -g nixbld -G nixbld -M -N -r -s "$(command -v nologin)" "nixbld$n"
 	done
 
-	echo "max-jobs = auto" | tee -a "$workdir/nix.conf" >/dev/null
-	echo "trusted-users = root $USER" | tee -a "$workdir/nix.conf" >/dev/null
-	echo "experimental-features = nix-command flakes" | tee -a "$workdir/nix.conf" >/dev/null
-
 	curl -o /tmp/nix-install.sh -L https://nixos.org/nix/install
 	chmod +x /tmp/nix-install.sh
-	/tmp/nix-install.sh --no-daemon --nix-extra-conf-file "$workdir/nix.conf"
+	/tmp/nix-install.sh --no-daemon
 	. /home/codespace/.nix-profile/etc/profile.d/nix.sh
-	nix build --no-link '.#homeConfigurations.codespace.activationPackage'
-	"$(nix path-info '.#homeConfigurations.codespace.activationPackage')"/activate
+	nix build --no-link --extra-experimental-features nix-command '.#homeConfigurations.codespace.activationPackage'
+	"$(nix path-info --extra-experimental-features nix-command '.#homeConfigurations.codespace.activationPackage')"/activate
 else
 	${SCRIPT_DIR}/scripts/install-nix.sh
 fi
