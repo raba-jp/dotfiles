6383c0d6169dbdb865532f5597a0988769d0046d
Hiroki Sakuraba <feath.7615@gmail.com>
2022-07-12 02:26:42 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -8,6 +8,16 @@ SCRIPT_DIR=$(
 ${SCRIPT_DIR}/scripts/install-nix.sh
 
 if [ "$CODESPACES" = "true" ]; then
+	mkdir -m 0755 /nix
+	chown codespace /nix
+	groupadd nixbld
+
+	for n in $(seq 1 10); do
+		useradd -c "Nix build user $n" -d /var/empty -g nixbld -G nixbld -M -N -r -s "$(command -v nologin)" "nixbld$n"
+	done
+
+	curl -o /tmp/nix-install.sh -L https://nixos.org/nix/install
+	/tmp/nix-install.sh --no-daemon
 	. /home/codespace/.nix-profile/etc/profile.d/nix.sh
 	nix build --no-link '.#homeConfigurations.codespace.activationPackage'
 	"$(nix path-info '.#homeConfigurations.codespace.activationPackage')"/activate
