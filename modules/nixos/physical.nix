ded12298460ea5f3272ba2dbdbf5155beb7eeedd
Hiroki Sakuraba <feath.7615@gmail.com>
2022-10-03 20:57:24 +0900

    Update

--- modules/nixos/physical.nix
+++ modules/nixos/physical.nix
@@ -57,8 +57,20 @@ in {
       };
     };
 
+    # Sound
     sound.enable = true;
-    hardware.pulseaudio.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
 
     fonts = {
       fontDir.enable = true;
