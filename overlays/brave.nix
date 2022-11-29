15eab32ed3554445ea662f8500b268129b8d6cdd
Hiroki Sakuraba <feath.7615@gmail.com>
2022-11-29 16:21:02 +0900

    Update

--- overlays/brave.nix
+++ overlays/brave.nix
@@ -1,10 +1,10 @@
 final: prev: {
   brave = prev.brave.overrideAttrs (old: rec {
-    version = "1.45.127";
+    version = "1.45.133";
 
     src = prev.fetchurl {
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
-      sha256 = "0xm2f5rq7a6s18754x7b2qsnp7wpp3l9ip1z8brfm8klp3lp287w";
+      sha256 = "0xfzvcvdvkyqp1sbkr4md6nij4jp626112ainnybsdcm5zazinwa";
     };
   });
 }
