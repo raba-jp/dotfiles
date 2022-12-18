ea3d13c1ed5a1a8bf9043910782c6bb457d91475
Hiroki Sakuraba <feath.7615@gmail.com>
2022-12-18 16:23:28 +0900

    Update

---
+++ home/features/hammerspoon/default.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  initFile = builtins.readFile ./init.lua;
+  cmdKanaEngFile = builtins.readFile ./Spoons/CMDKanaEng.spoon/init.lua;
+  ctrlMateFile = builtins.readFile ./Spoons/CtrlMate.spoon/init.lua;
+in
+  lib.mkIf pkgs.stdenvNoCC.isDarwin {
+    home.file = {
+      ".hammerspoon/init.lua".text = initFile;
+      ".hammerspoon/Spoons/CMDKanaEng.spoon/init.lua".text =
+        cmdKanaEngFile;
+      ".hammerspoon/Spoons/CtrlMate.spoon/init.lua".text = ctrlMateFile;
+    };
+  }
