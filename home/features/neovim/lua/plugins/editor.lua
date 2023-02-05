9bda13738b1cdad6bd3a2e569eca3759e6d9a4b9
Hiroki Sakuraba <feath.7615@gmail.com>
2023-02-06 00:34:51 +0900

    Update

--- home/features/neovim/lua/plugins/editor.lua
+++ home/features/neovim/lua/plugins/editor.lua
@@ -30,19 +30,11 @@ return {
 			{
 				"<leader>fe",
 				function()
-					require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
-				end,
-				desc = "Explorer NeoTree (root dir)",
-			},
-			{
-				"<leader>fE",
-				function()
 					require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
 				end,
 				desc = "Explorer NeoTree (cwd)",
 			},
-			{ "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
-			{ "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+			{ "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
 		},
 		opts = {
 			close_if_last_window = false,
