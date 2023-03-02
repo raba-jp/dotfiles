906dd43e34ca2b0deac7937c1988853e46f08274
Hiroki Sakuraba <feath.7615@gmail.com>
2023-03-03 01:09:32 +0900

    Update

--- nixos/hosts/xps/default.nix
+++ nixos/hosts/xps/default.nix
@@ -25,33 +25,52 @@
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  services = {
-    udev.extraRules = ''
-      KERNEL=="hidraw*", ATTRS{idVendor}=="bb01", MODE="0664", GROUP="users"
-      KERNEL=="hidraw*", ATTRS{idVendor}=="4653", MODE="0664", GROUP="users"
-    '';
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "ondemand";
+    powertop.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [
-    glxinfo
-    libva-utils
-    vulkan-tools
-    google-chrome
-    obsidian
-    vscode
-    papirus-icon-theme
-    gparted
-    flutter
-    xclip
-    dconf2nix
-    libnotify
-    lm_sensors
-    bottles
-    brave
-    slack
-    discord
-    virt-manager
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      glxinfo
+      libva-utils
+      vulkan-tools
+      google-chrome
+      obsidian
+      vscode
+      papirus-icon-theme
+      gparted
+      flutter
+      xclip
+      dconf2nix
+      libnotify
+      lm_sensors
+      bottles
+      brave
+      slack
+      discord
+      virt-manager
+    ];
+
+    persistence."/persistent" = {
+      hideMounts = true;
+      directories = [
+        "/var/log"
+        "/var/lib/bluetooth"
+        "/var/lib/nixos"
+        "/var/lib/systemd/coredump"
+        "/etc/NetworkManager/system-connections"
+      ];
+      files = [
+        "/etc/machine-id"
+        {
+          file = "/etc/ssh/ssh_host_ed25519_key";
+          parentDirectory = {mode = "u=rwx,g=,o=";};
+        }
+      ];
+    };
+  };
 
   hardware.opengl = {
     enable = true;
