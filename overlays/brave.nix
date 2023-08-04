aeffa910b3c4bc6689d84b0309bb8b74f60d1b6c
raba-jp <raba-jp@users.noreply.github.com>
2023-08-04 15:05:45 +0000

    [create-pull-request] automated change

--- overlays/brave.nix
+++ overlays/brave.nix
@@ -1,11 +1,11 @@
 # Auto generated
 _: prev: {
   brave = prev.brave.overrideAttrs (_old: {
-    version = "1.56.9";
+    version = "1.56.20";
 
     src = prev.fetchurl {
-      url = "https://github.com/brave/brave-browser/releases/download/v1.56.9/brave-browser_1.56.9_amd64.deb";
-      sha256 = "1ydv3l6gv24m2i9hy66dz7v4jzcxzl98dn8ixg61yq5h8z2ka3kk";
+      url = "https://github.com/brave/brave-browser/releases/download/v1.56.20/brave-browser_1.56.20_amd64.deb";
+      sha256 = "1smiqsbn1l0d1lxc5zbqv54mm5kq5ss0cqhq95y7bfq5il03igmr";
     };
   });
 }
