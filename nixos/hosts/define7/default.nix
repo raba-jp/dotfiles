5b3df98a6b56bb127378f3f7b1319ef06d411657
Hiroki Sakuraba <feath.7615@gmail.com>
2023-10-31 19:41:10 +0900

    Update

--- nixos/hosts/define7/default.nix
+++ nixos/hosts/define7/default.nix
@@ -7,7 +7,7 @@
   imports = [
     inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
     inputs.nixos-hardware.nixosModules.common-pc-ssd
     inputs.disko.nixosModules.disko
     inputs.impermanence.nixosModules.impermanence
